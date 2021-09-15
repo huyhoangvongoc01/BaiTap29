8325bda5d8cbfdb02573cfa2a826e062e9b0c7e7
Admin <Admin@DESKTOP-QCGJ9CH>
2021-09-15 15:42:58 +0700

    BaiTap1

--- BaiTap/src/Cacbaitap/BaiTap1.java
+++ BaiTap/src/Cacbaitap/BaiTap1.java
@@ -1,17 +1,20 @@
 package Cacbaitap;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class BaiTap1 {
 	public static void main(String[] args) {
-		int number1, number2;
-		System.out.println("Nhap vao 2 so:");
-		Scanner sc = new Scanner(System.in);
-		number1 = sc.nextInt();
-		number2 = sc.nextInt();
-	    if(number1 >= number2)
-	    	System.out.println( +number1 + "-" +number2 + " Dung");
+		Scanner input = new Scanner(System.in);
+		int a = (int)(Math.random() * 30) +0;
+		int b = (int)(Math.random() * 30) + a; 
+		int c = b - a;
+	    System.out.println("Ket qua cua phep toan " + b + "-" + a +" la gi:");
+	    System.out.println("Nhap dap an cua ban");
+	    int x = input.nextInt();
+	    if(x==c)
+	    	System.out.println( " Correct");
 	    else
-	    	System.out.println( +number1 + "-" +number2 + " Sai");
+	    	System.out.println(" Wrong");
 	}
 }
